d96d4fd0fe20aff43f71ae36e37af0cfa5e2a386
Lucille <lchoisnet@umanit.fr>
2022-12-01 20:51:35 +0100

    Day 1
    - Init commit
    - First day challenge completed

---
+++ 1201/index.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+
+//////////
+// 1
+//////////
+$caloriesPerElf = [];
+
+foreach (explode(PHP_EOL.PHP_EOL, file_get_contents('./input.txt')) as $data) {
+    $caloriesPerElf[] = explode(PHP_EOL, $data);
+}
+
+array_walk($caloriesPerElf, function (&$value, $key) {
+    $value = array_sum($value);
+});
+
+dump(max($caloriesPerElf));
+
+//////////
+// 2
+//////////
+arsort($caloriesPerElf);
+$caloriesPerElf = array_values($caloriesPerElf);
+
+dump(array_sum(array_slice($caloriesPerElf, 0, 3)));
